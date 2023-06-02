cddcc02d765758eabb40bd123a80d82806ea808b
Sumit Prakash Shrestha <sshrestha@advalent.com>
2023-06-03 01:35:34 +0545

    Initial commit

---
+++ src/test/java/com/sumit/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumit.springapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
